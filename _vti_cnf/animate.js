c0742ed23d9ddcc0398715534a2200924f72682f
Chris Baker <chris.baker@thetradedesk.com>
2026-03-31 16:37:46 -0400

    New Website

--- _vti_cnf/animate.js
+++
@@ -1,8 +0,0 @@
-vti_encoding:SR|utf8-nl
-vti_author:SR|User
-vti_modifiedby:SR|User
-vti_timecreated:TR|21 Apr 2005 21:07:40 -0000
-vti_timelastmodified:TR|21 Apr 2005 21:07:40 -0000
-vti_backlinkinfo:VX|examples.htm index.html contact.html
-vti_extenderversion:SR|4.0.2.3406
-vti_syncwith_secondwindmusic.com\:80:TR|21 Apr 2005 21:07:40 -0000
